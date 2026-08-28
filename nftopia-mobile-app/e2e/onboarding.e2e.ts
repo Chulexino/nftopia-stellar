801e6084ab700807fec18e64b59f57533affce10
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2026-08-28 19:25:23 +0100

    test: type Detox onboarding specs

--- nftopia-mobile-app/e2e/onboarding.e2e.ts
+++ nftopia-mobile-app/e2e/onboarding.e2e.ts
@@ -1,3 +1,5 @@
+import { by, device, element, expect } from 'detox';
+
 describe('onboarding and wallet import', () => {
   beforeAll(async () => {
     await device.launchApp({ newInstance: true });
